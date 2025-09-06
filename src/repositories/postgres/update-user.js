6dc9277c1c0177b114827d022e02d942c2959551
Leosampaiou <leonardo.sampaiou@gmail.com>
2025-09-06 17:15:05 -0300

    feat: implement UpdateUserRepository for updating user details in the database

---
+++ src/repositories/postgres/update-user.js
@@ -0,0 +1,28 @@
+import { PostgresHelper } from '../../db/postgres/helper'
+
+export class UpdateUserRepository {
+    async execute(id, updateUseParams) {
+        const updateFilds = []
+        const updateValues = []
+
+        Object.keys(updateUseParams).forEach((key) => {
+            updateFilds.push(`${key} = $${updateValues.length + 1}`)
+            updateValues.push(updateUseParams[key])
+        })
+
+        updateValues.push(id)
+
+        const updateQuery = `
+            UPDATE users
+            SET ${updateFilds.join(', ')}
+            WHERE id = $${updateValues.length}
+            RETURNING * `
+
+        const updatedUser = await PostgresHelper.query(
+            updateQuery,
+            updateValues,
+        )
+
+        return updatedUser[0]
+    }
+}
