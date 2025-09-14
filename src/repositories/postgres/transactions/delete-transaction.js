be620403ea189eece2b46a7aa24ae5209806cb80
Leosampaiou <leonardo.sampaiou@gmail.com>
2025-09-14 18:50:49 -0300

    feat: add PostgresDeleteTransactionRepository

---
+++ src/repositories/postgres/transactions/delete-transaction.js
@@ -0,0 +1,11 @@
+import { PostgresHelper } from '../../../db/postgres/helper'
+
+export class PostgresDeleteTransactionRepository {
+    async execute(id) {
+        const deleteTransaction = await PostgresHelper.query(
+            'DELETE FROM transactions WHERE id = $1 RETURNING *',
+            [id],
+        )
+        return deleteTransaction[0]
+    }
+}
