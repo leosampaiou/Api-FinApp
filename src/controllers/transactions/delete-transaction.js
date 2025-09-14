9ac47cd4210f2bf56f19a94ad5003db4c88e27c2
Leosampaiou <leonardo.sampaiou@gmail.com>
2025-09-14 19:04:02 -0300

    feat: add DeleteTransactionController

---
+++ src/controllers/transactions/delete-transaction.js
@@ -0,0 +1,33 @@
+import {
+    genetateInvalidIdResponse,
+    serverError,
+    ok,
+    checkIfIdIsValid,
+} from '../helpers'
+
+export class deletedTransactionController {
+    constructor(deleteTransactionUseCase) {
+        this.deleteTransactionUseCase = deleteTransactionUseCase
+    }
+    async execute(httpRequest) {
+        try {
+            const id = httpRequest.params.id
+
+            const transactionIdIsValid = checkIfIdIsValid(id)
+            if (!transactionIdIsValid) {
+                return genetateInvalidIdResponse()
+            }
+            const deletedTransaction =
+                await this.deleteTransactionUseCase.execute(id)
+
+            if (!deletedTransaction) {
+                return genetateInvalidIdResponse()
+            }
+
+            return ok(deletedTransaction)
+        } catch (error) {
+            console.error(error)
+            return serverError()
+        }
+    }
+}
