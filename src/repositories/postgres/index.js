6b9d386b2590148da3b5863324ec21816004f7cb
Leosampaiou <leonardo.sampaiou@gmail.com>
2025-09-14 23:03:05 -0300

    feat: add PostgresGetUserBalanceRepository

--- src/repositories/postgres/index.js
+++ src/repositories/postgres/index.js
@@ -3,6 +3,7 @@ export * from './user/get-user-by-id.js'
 export * from './user/update-user.js'
 export * from './user/get-user-by-email.js'
 export * from './user/delete-user.js'
+export * from './user/get-user-balance.js'
 
 export * from './transactions/create-transactions.js'
 export * from './transactions/get-transaction-by-user-id.js'
