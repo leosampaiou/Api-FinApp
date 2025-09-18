3ba6627bbaac28152f6db423335795bbb38d83bd
Leosampaiou <leonardo.sampaiou@gmail.com>
2025-09-18 11:37:27 -0300

    feat: add zod to create user controller

--- src/controllers/user/create-user.js
+++ src/controllers/user/create-user.js
@@ -1,65 +1,35 @@
+import { badRequest, created } from '../helpers/index.js'
 import { EmailAlreadyInUseError } from '../../errors/user.js'
-import {
-    badRequest,
-    serverError,
-    created,
-    checkIfPasswordIsValid,
-    checkIfEmailIsValid,
-    genetateinvalidPasswordResponse,
-    genetateEmailAlredyInUseResponse,
-    validatedRequiredFields,
-    genetateRequairedFieldResponse,
-} from '../helpers/index.js'
-
+import { ZodError } from 'zod'
+import { createUserSchema } from '../../schema/index.js'
 export class CreateUserCotroller {
     constructor(createUserUseCase) {
         this.createUserUseCase = createUserUseCase
     }
+
     async execute(httpRequest) {
         try {
             const params = httpRequest.body
-
-            const requireFields = [
-                'first_name',
-                'last_name',
-                'email',
-                'password',
-            ]
-            const requiredFieldsValidation = validatedRequiredFields(
-                params,
-                requireFields,
-            )
-
-            if (!requiredFieldsValidation.ok) {
-                return genetateRequairedFieldResponse(
-                    requiredFieldsValidation.missingField,
-                )
-            }
-
-            const passwordIsValid = checkIfPasswordIsValid(params.password)
-
-            if (!passwordIsValid) {
-                return genetateinvalidPasswordResponse()
-            }
-
-            const emailIsValid = checkIfEmailIsValid(params.email)
-
-            if (!emailIsValid) {
-                return genetateEmailAlredyInUseResponse()
-            }
+            await createUserSchema.parseAsync(params)
 
             const createdUser = await this.createUserUseCase.execute(params)
 
             return created({
-                menssage: 'User created successfully',
+                message: 'User created successfully',
                 user: createdUser,
             })
         } catch (error) {
-            console.error(error)
-            if (error instanceof EmailAlreadyInUseError) {
-                return badRequest({ menssage: error.message })
+            console.log(error)
+
+            if (error instanceof ZodError) {
+                return badRequest({
+                    message: error.issues?.[0]?.message,
+                })
             }
-            return serverError()
+
+            if (error instanceof EmailAlreadyInUseError) {
+                return badRequest({ message: error.message })
+            }
         }
     }
 }
---
+++ src/schema/user.js
@@ -0,0 +1,23 @@
+import { z } from 'zod'
+
+export const createUserSchema = z.object({
+    first_name: z.string().trim().min(1, {
+        message: 'First name is required',
+    }),
+    last_name: z.string().trim().min(1, {
+        message: 'Last name is required',
+    }),
+    email: z
+        .string()
+        .trim()
+        .min(1, {
+            message: 'Email is required',
+        })
+        .check(z.email()),
+    password: z
+        .string({
+            required_error: 'Password is required',
+        })
+        .trim()
+        .min(6),
+})
