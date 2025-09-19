d99df19b0394a5622eb7cf8ad0b6d7a36299fb98
Leosampaiou <leonardo.sampaiou@gmail.com>
2025-09-19 18:57:04 -0300

    fix: passing of userId from controller to use case

--- src/use-cases/transactions/get-transaction-by-user-id.js
+++ src/use-cases/transactions/get-transaction-by-user-id.js
@@ -1,19 +1,19 @@
 import { UserNotFoundError } from '../../errors/user.js'
-
 export class GetTransactionByUserIdUseCase {
     constructor(getUserByIdRepository, getTransactionByUserIdRepository) {
         this.getUserByIdRepository = getUserByIdRepository
         this.getTransactionByUserIdRepository = getTransactionByUserIdRepository
     }
-    async execute(params) {
-        const user = await this.getUserByIdRepository.execute(params.userId)
+
+    async execute(userId) {
+        const user = await this.getUserByIdRepository.execute(userId)
 
         if (!user) {
-            throw new UserNotFoundError(params.userId)
+            throw new UserNotFoundError(userId)
         }
 
         const transactions =
-            await this.getTransactionByUserIdRepository.execute(params.userId)
+            await this.getTransactionByUserIdRepository.execute(userId)
 
         return transactions
     }
