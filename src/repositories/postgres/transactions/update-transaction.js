a126b08ab73233afec43e05b8e4cf47d41886f9f
Leosampaiou <leonardo.sampaiou@gmail.com>
2025-09-19 19:19:07 -0300

    refactor: migrate PostgresUpdateTransactionRepository to Prisma

--- src/repositories/postgres/transactions/update-transaction.js
+++ src/repositories/postgres/transactions/update-transaction.js
@@ -1,28 +1,12 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js'
-
+import { prisma } from '../../../../prisma/prisma.js'
 export class PostgresUpdateTransactionRepository {
     async execute(transactionId, updateTransactionParams) {
-        const updateFilds = []
-        const updateValues = []
-
-        Object.keys(updateTransactionParams).forEach((key) => {
-            updateFilds.push(`${key} = $${updateValues.length + 1}`)
-            updateValues.push(updateTransactionParams[key])
+        const updatedTransaction = prisma.transaction.update({
+            where: {
+                id: transactionId,
+            },
+            data: updateTransactionParams,
         })
-
-        updateValues.push(transactionId)
-
-        const updateQuery = `
-                UPDATE transactions
-                SET ${updateFilds.join(', ')}
-                WHERE id = $${updateValues.length}
-                RETURNING * `
-
-        const updatedTransaction = await PostgresHelper.query(
-            updateQuery,
-            updateValues,
-        )
-
-        return updatedTransaction[0]
+        return updatedTransaction
     }
 }
