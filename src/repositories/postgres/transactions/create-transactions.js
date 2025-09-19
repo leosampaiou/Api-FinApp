516b76a1e513159785dd76abf690c768132956b3
Leosampaiou <leonardo.sampaiou@gmail.com>
2025-09-19 18:58:17 -0300

    refactor: migrate PostgresCreateTransactionRepository to Prisma

--- src/repositories/postgres/transactions/create-transactions.js
+++ src/repositories/postgres/transactions/create-transactions.js
@@ -1,18 +1,18 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js'
+import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresCreateTransactionsRepository {
     async execute(createTransactionParams) {
-        const createTransaction = await PostgresHelper.query(
-            'INSERT INTO transactions (id, user_id, name, date, amount, type) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
-            [
-                createTransactionParams.id,
-                createTransactionParams.user_id,
-                createTransactionParams.name,
-                createTransactionParams.date,
-                createTransactionParams.amount,
-                createTransactionParams.type,
-            ],
-        )
-        return createTransaction[0]
+        const createTransaction = await prisma.transaction.create({
+            data: {
+                name: createTransactionParams.name,
+                date: createTransactionParams.date,
+                amount: createTransactionParams.amount,
+                type: createTransactionParams.type,
+                user: {
+                    connect: { id: createTransactionParams.user_id },
+                },
+            },
+        })
+        return createTransaction
     }
 }
