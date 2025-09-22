44858d70a4f9b88a85a74a3b4830be4716fca846
Leosampaiou <leonardo.sampaiou@gmail.com>
2025-09-22 14:52:47 -0300

    test: add validation test for missing last_name returning 400

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -41,3 +41,20 @@ test('should return 400 Bad Request if first_name is missing', async () => {
 
     expect(result.statusCode).toBe(400)
 })
+
+test('should return 400 bad request if last_name is missing', async () => {
+    const createUserUseCaseStub = new CreateUserUseCaseStub()
+    const createUserCotroller = new CreateUserCotroller(createUserUseCaseStub)
+
+    const httpRequest = {
+        body: {
+            first_name: 'leo',
+            email: 'leo@gmail.com',
+            password: '123456789',
+        },
+    }
+
+    const result = await createUserCotroller.execute(httpRequest)
+
+    expect(result.statusCode).toBe(400)
+})
