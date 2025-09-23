bfd51dd77ecf5b51cff6f860854c7bfbefb219e9
Leosampaiou <leonardo.sampaiou@gmail.com>
2025-09-23 17:25:10 -0300

    test: ensure delete user controller returning 404 if the user id does not exist

--- src/controllers/user/delete-user.test.js
+++ src/controllers/user/delete-user.test.js
@@ -1,5 +1,6 @@
 import { faker } from '@faker-js/faker'
 import { DeleteUserController } from './delete-user'
+import { UserNotFoundError } from '../../errors/user'
 
 describe('DeleteUserController', () => {
     class DeleteUserUseCaseStub {
@@ -37,5 +38,17 @@ describe('DeleteUserController', () => {
         })
 
         expect(result.statusCode).toBe(400)
+    })
+
+    test('should return 404 not found if user id does not exist', async () => {
+        const { sut, deleteUserUseCase } = makeSut()
+
+        jest.spyOn(deleteUserUseCase, 'execute').mockImplementationOnce(() => {
+            throw new UserNotFoundError()
+        })
+
+        const result = await sut.execute(httpRequest)
+
+        expect(result.statusCode).toBe(404)
     })
 })
