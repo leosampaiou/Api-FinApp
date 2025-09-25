2a80d81f29109f25a590ce94e81b7081e7ef27d7
Leosampaiou <leonardo.sampaiou@gmail.com>
2025-09-25 13:40:06 -0300

    test: ensure getUserById returns 200 when request is successful

---
+++ src/controllers/user/get-user-by-id.test.js
@@ -0,0 +1,28 @@
+import { faker } from '@faker-js/faker'
+import { GetUserByIdController } from './get-user-by-id'
+
+describe('GetUserByIdController', () => {
+    class GetUserByIdUseCaseStub {
+        async execute(user) {
+            return user
+        }
+    }
+    const httpRequest = {
+        params: { userId: faker.string.uuid() },
+    }
+
+    const makeSut = () => {
+        const getUserByIdUseCase = new GetUserByIdUseCaseStub()
+        const sut = new GetUserByIdController(getUserByIdUseCase)
+
+        return { sut, getUserByIdUseCase }
+    }
+
+    test('should return 200 and successfully get the user', async () => {
+        const { sut } = makeSut()
+
+        const result = await sut.execute(httpRequest)
+
+        expect(result.statusCode).toBe(200)
+    })
+})
